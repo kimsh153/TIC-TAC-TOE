15cd9c96216a43421c524c5fc0342bac82f2c921
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-07-19 23:49:12 +0900

    :truck: Component 별로 분리

---
+++ src/Board.tsx
@@ -0,0 +1,93 @@
+import Square from "./Square";
+
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      squares[a] = "☐";
+      squares[b] = "☐";
+      squares[c] = "☐";
+      return true;
+    }
+  }
+  return null;
+}
+
+type BoardValue = {
+  xIsNext: boolean;
+  squares: Array<string>;
+  onPlay: (value: Array<string>) => void;
+  moves: number;
+};
+
+function Board({ xIsNext, squares, onPlay, moves }: BoardValue) {
+  const handleClick = (i: number) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  };
+
+  let status;
+  if (moves < 9) {
+    if (calculateWinner(squares)) {
+      status = "Winner: " + (xIsNext ? "O" : "X");
+    } else {
+      status = "Next Player: " + (xIsNext ? "X" : "O");
+    }
+  } else {
+    status = "Draw!!";
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="flex items-center">
+        {[0, 1, 2].map((index, idx) => (
+          <Square
+            key={idx}
+            value={squares[index]}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+      <div className="flex items-center">
+        {[3, 4, 5].map((index, idx) => (
+          <Square
+            key={idx}
+            value={squares[index]}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+      <div className="flex items-center">
+        {[6, 7, 8].map((index, idx) => (
+          <Square
+            key={idx}
+            value={squares[index]}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Board;
