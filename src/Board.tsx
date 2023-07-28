7c0ffb93f64c3672e6975503d9266e983ff02a72
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-07-29 03:30:10 +0900

    :fire: 테스트 및 디버깅용 코드 지우기

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,16 +1,5 @@
 import Square from "./Square";
 
-const lines = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
 type BoardValue = {
   xIsNext: boolean;
   squares: Array<string>;
@@ -21,38 +10,14 @@ type BoardValue = {
 
 function Board({ xIsNext, squares, onPlay, moves, isEnd }: BoardValue) {
   let status;
-  let win = moves >= 6;
 
   const handleClick = (i: number) => {
     if (squares[i] || isEnd) {
       return;
     }
-    // console.log(isEnd);
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O";
     onPlay(nextSquares);
-  };
-
-  const calculateWinner = (squares: string[]) => {
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        const calculatedSquares = squares.slice();
-        calculatedSquares[a] = "☐";
-        calculatedSquares[b] = "☐";
-        calculatedSquares[c] = "☐";
-        win = true;
-        // isEnd = true;
-        // console.log("Asdf", isEnd);
-        return calculatedSquares;
-      }
-    }
-    win = false;
-    return squares;
   };
 
   if (isEnd) {
